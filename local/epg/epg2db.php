b375e94fab0e65f2e91c1a11e5aa941778967066
Uncle Fedor <uncle.f@writeme.com>
2017-07-10 22:43:40 +0000

    Fully functional EPG importer

--- local/epg/epg2db.php
+++ local/epg/epg2db.php
@@ -1,5 +1,20 @@
-#!/usr/local/bin/php
-<?
+<?php
+
+error_reporting(E_ALL|E_STRICT);
+ini_set('display_errors', 1);
+ini_set('error_log', '/var/www/log/php-cli.log');
+
+define('USAGE', "\nUsage:\n\t" . basename(__FILE__) . " <epg.gz> <db.sqlite>\n\n");
+
+$epg_gz = $argv[1] ?? '';
+$db_file = $argv[2] ?? 0;
+
+// check if no arguments supplied from command line
+if (!$epg_gz || !$db_file)
+{
+    fwrite(STDERR, USAGE);
+    exit(1);
+}
 
 $programme_opening_tag = '@';
 $programme_closing_tag = '</programme>';
@@ -8,7 +23,7 @@ print("\nImporting data into SQLite database...\n");
 
 try
 {
-    $db = new SQLite3('/home/sboxx.org/epg/db.sqlite', SQLITE3_OPEN_READWRITE);
+    $db = new SQLite3($db_file, SQLITE3_OPEN_READWRITE);
 }
 catch (Exception $exception)
 {
@@ -16,7 +31,7 @@ catch (Exception $exception)
     die(sprintf("Error: cannot connect to SQLite database\n%s\n", $exception->getMessage()));
 }
 
-$tv_guide_file = gzopen('/home/sboxx.org/tmp/tvprogram_ua_ru.gz', 'r')
+$tv_guide_file = gzopen($epg_gz, 'r')
     or die("Could not open program guide .gz file\n");
 
 $buffer = '';
@@ -53,14 +68,23 @@ while (!gzeof($tv_guide_file))
         if ($closing_tag_position !== FALSE)
         {
             preg_match('@start="(.+)"@U', $buffer, $match);
+
             $start_datetime = !empty($match[1]) ? $match[1] : '';
+
             preg_match('@stop="(.+)"@U', $buffer, $match);
+
             $stop_datetime = !empty($match[1]) ? $match[1] : '';
+
             preg_match('@channel="(.+)"@U', $buffer, $match);
+
             $channel_id = !empty($match[1]) ? $match[1] : '';
+
             preg_match('@<title.*>(.+)</title>@U', $buffer, $match);
+
             $title = !empty($match[1]) ? $match[1] : '';
+
             preg_match('@<desc.*>(.+)</desc>@U', $buffer, $match);
+
             $desc = !empty($match[1]) ? $match[1] : '';
 
             $buffer = substr($buffer, $closing_tag_position + 12);
@@ -73,20 +97,27 @@ while (!gzeof($tv_guide_file))
                 $title = str_replace('&quot;', '"', $title);
                 $title = preg_replace('/ *\(. категория\).?/', '', $title);
                 $desc = str_replace('&quot;', '"', $desc);
+
                 if ($title{0} == '"' && strpos($title, '".') !== FALSE)
                 {
-                    $title = str_replace(array('"', '".'), '', $title);
+                    $title = str_replace(['"', '".'], '', $title);
                 }
+
                 if (substr($title,-1) == '.')
                 {
                     $title = substr($title,0,-1);
                 }
+
                 $title = $db->escapeString($title);
                 $desc = $db->escapeString($desc);
                 $channel_id = $db->escapeString($channel_id);
+
                 $_sql = "INSERT INTO epg (ch_id,start_time,end_time,event_name,event_descr) VALUES ('%s',%u,%u,'%s','%s')";
+
                 $sql = sprintf($_sql, $channel_id, $start_time, $stop_time, $title, $desc);
+
                 $result = $db->exec($sql);
+
                 if ($result)
                 {
                     $i++;
