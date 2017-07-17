63d802811842b3a7d9a56a747d61fa8cd0f8b362
Uncle Fedor <uncle.f@writeme.com>
2017-07-17 16:21:42 +0000

    Reorganised website's main index page into sections

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = '/var/www/html/dbdat.zip';
+$db = './dbdat.zip';
 $dd = file_exists($db) ? date('d.m.Y', filemtime($db)) : "";
 
 ?><!DOCTYPE html>
@@ -12,32 +12,38 @@ $dd = file_exists($db) ? date('d.m.Y', filemtime($db)) : "";
     <link rel="stylesheet" href="index.css" />
 </head>
 <body>
-    <div id="logo"><img src="sputnik.jpg" alt="Sputnik"></div>
+    <aside><a title="PGI Archive" href="https://mega.nz/#F!kKgzDAhJ!kZTL3xQcEgz_8fqLYC2fAA"><img src="sputnik.jpg" alt="Sputnik"></a></aside>
+    <main>
     <header>
         <h1>Добро пожаловать на сайт проекта PGI для IPBOX HD!</h1>
         <p>Здесь вы сможете найти полезные приложения, скрипты и ссылки для вашего устаревшего ресивера Cuberevo/IPBox/Sezam HD.</p>
         <p>ВНИМАНИЕ! Большинство материалов этого и других упомянутых сайтов касается работы с <strong>прошивкой PGI Final</strong>!</p>
     </header>
     <section>
+        <h2>База данных db.dat (спутники и транспондеры):</h2>
         <ul>
-<?php if ($dd) { ?>
-            <li><a href="dbdat.zip?<?=$dd?>">Чистая база данных db.dat</a> с обновленным списком спутников и транспондеров<?=$dd ? " от $dd" : ""?></li>
-<?php } ?>
-            <li><a href="dbdat.txt?<?=date('d.m.Y')?>">История изменений базы данных db.dat</a> обновляется вместе с базой ежедневно в 00:00 <?=date('T')?> </li>
-            <li><a href="/epg/">Программа передач</a> из Интернета для прошивок PGI - интерфейс для настройки</li>
-            <li><a href="epg/pgi_epg.zip">Скрипт получения ТВ Гида</a> из Интернета (используется после настройки через линк выше)</li>
-            <li><a href="piconid.zip">Скрипт идентификации пиконов</a>, нужен для того чтобы правильно назвать файлы с пиконами для прошивки PGI</li>
-            <li><a href="multiboot/">Файлы конфигурации мультибута</a>, которые также идут в комплекте с прошивкой PGI</li>
+            <li><a href="dbdat.txt">История изменений базы данных db.dat</a> обновляется вместе с базой ежедневно в 00:00 UTC</li>
+            <li><a href="dbdat.zip">Чистая база данных db.dat</a> с обновленным списком спутников и транспондеров<?=$dd ? " от $dd" : ""?></li>
         </ul>
-        <p>Полезные ссылки на другие ресурсы, рекомендуемые к использованию:</p>
+    </section>
+    <section>
+        <h2>Программа передач для прошивок PGI на основе XMLTV</h2>
         <ul>
-            <li><a title="https://mega.nz/#F!8BVVXCYB!RJq0aUtK_eQZGxoSZdA3Dw" href="https://mega.nz/#F!8BVVXCYB!RJq0aUtK_eQZGxoSZdA3Dw">Прошивки PGI</a> для всех поддерживаемых моделей ресиверов</li>
-            <li>Домашняя страница программы для редактирования базы каналов <a href="http://www.pceditor.de">PC Editor</a>. Также, версия 1.2.60 доступна для скачивания <a href="Setup_PCEditor_1.2.60.zip">здесь</a>.</li>
+            <li><a href="/epg/">Веб интерфейс для начальной настройки</a></li>
+            <li><a href="https://mega.nz/#!pO43VZqA!HuSn4IJEDx-ERETfTwmA0fllJx0-8_WI5tfPk4gi_Uc">Скрипт для обновления данных EPG для прошивки PGI</a></li>
+        </ul>
+    </section>
+    <section>
+        <h2>Полезные ссылки на другие ресурсы, рекомендуемые к использованию:</h2>
+        <ul>
+            <li><a title="https://mega.nz/#F!kKgzDAhJ!kZTL3xQcEgz_8fqLYC2fAA" href="https://mega.nz/#F!kKgzDAhJ!kZTL3xQcEgz_8fqLYC2fAA">Файловый архив PGI</a> &mdash; прошивки PGI, файлы конфигурации мультибут и скрипты для EPG и пиконов.</li>
+            <li>Домашняя страница программы для редактирования базы каналов <a href="http://www.pceditor.de">PC Editor</a>.<br /> Также, версия 1.2.60 доступна для скачивания из файлового репозитория по ссылке выше.</li>
             <li>Русскоязычный <a href="http://gomel-sat.bz/forum/21-ipbox-hd-sezam-hd-cuberevo-hd/">интернет-форум Gomel-Sat</a>, где тусуются пользователи ресиверов Cuberevo (и не только).</li>
         </ul>
     </section>
     <footer>
-        Последнее обновление сайта: 18 июня 2017
+        Последнее обновление сайта: 17 июля 2017
     </footer>
+    </main>
 </body>
 </html>
